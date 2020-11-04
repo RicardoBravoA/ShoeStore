3d6ce6a78a17ec674520a0d3ff6ce82f99416d44
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-04 01:00:00 -0500

    wip: Add views to fragment login

--- app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -23,7 +23,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        view.findViewById<Button>(R.id.login_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
