e69da769effe20e800c3b18a2896d554ab256753
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-04 02:15:11 -0500

    refactor: Remove unused lines

--- app/src/main/java/com/udacity/shoestore/login/LoginViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/login/LoginViewModel.kt
@@ -1,17 +1,11 @@
 package com.udacity.shoestore.login
 
-import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.udacity.shoestore.utils.InputUtils
 
 class LoginViewModel : ViewModel() {
-
-    init {
-        Log.i("LoginViewModel", "init")
-    }
 
     private val _validateData = MutableLiveData<Boolean>()
     val validateData: LiveData<Boolean>
@@ -24,16 +18,6 @@ class LoginViewModel : ViewModel() {
     private val _validatePwd = MutableLiveData<Boolean>()
     val validatePwd: LiveData<Boolean>
         get() = _validatePwd
-
-    val emailAddress = MutableLiveData<String>("")
-
-    /*val valid = MediatorLiveData<Boolean>().apply {
-        addSource(emailAddress) {
-            val valid = InputUtils.isValidEmail(it)
-            Log.d(it, valid.toString())
-            value = valid
-        }
-    }*/
 
     fun validateData(email: String, pwd: String) {
         _validateEmail.value = InputUtils.isValidEmail(email)
