90b4c0a89cb7ebfe06dd41df5b682fe4c5c6c003
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-04 20:01:40 -0500

    wip: Show data in recycler view

--- app/src/main/java/com/udacity/shoestore/utils/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/com/udacity/shoestore/utils/RecyclerViewBindingAdapter.kt
@@ -3,7 +3,7 @@ package com.udacity.shoestore.utils
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-@BindingAdapter(value = ["setAdapter"])
+@BindingAdapter(value = ["adapter"])
 fun RecyclerView.bindRecyclerViewAdapter(adapter: RecyclerView.Adapter<*>) {
     this.run {
         this.setHasFixedSize(true)
