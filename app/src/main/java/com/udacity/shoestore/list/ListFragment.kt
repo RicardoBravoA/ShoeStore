c48b1105fa54a4ef55c3f4c9a521bbc3dfecf045
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-05 03:29:36 -0500

    wip: Add navigation between detail and list fragment

--- app/src/main/java/com/udacity/shoestore/list/ListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/list/ListFragment.kt
@@ -1,6 +1,7 @@
 package com.udacity.shoestore.list
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -45,6 +46,13 @@ class ListFragment : Fragment() {
                 }
             }
         })
+
+        val shoe = arguments?.getParcelable<ShoeModel>("shoe")
+
+        shoe?.let {
+            Log.i("z- shoe", shoe.toString())
+        }
+
 
         return binding.root
     }
