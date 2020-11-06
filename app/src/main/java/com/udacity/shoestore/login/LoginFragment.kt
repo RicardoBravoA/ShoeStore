6a4cc1b502a499b605a9de6dcb69f55f54cfaa20
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-06 00:46:41 -0500

    refactor: Remove error messages

--- app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.udacity.shoestore.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -69,14 +70,16 @@ class LoginFragment : Fragment() {
         }
 
         binding.emailEdit.addTextChangedListener(CustomTextWatcher(
-            onChanged = { _, _, _, _ ->
+            onChanged = { value, _, _, _ ->
                 binding.emailTextInputLayout.validateErrorInputLayout()
+                viewModel.validateEmail(value.toString())
             }
         ))
 
         binding.pwdEdit.addTextChangedListener(CustomTextWatcher(
-            onChanged = { _, _, _, _ ->
+            onChanged = { value, _, _, _ ->
                 binding.pwdTextInputLayout.validateErrorInputLayout()
+                viewModel.validatePwd(value.toString())
             }
         ))
 
