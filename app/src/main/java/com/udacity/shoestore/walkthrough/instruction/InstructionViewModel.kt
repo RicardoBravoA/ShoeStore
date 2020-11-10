148a978b9976eeb3db1b80a1c12a316706e3de1b
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-09 21:15:49 -0500

    refactor: Update instruction view

--- app/src/main/java/com/udacity/shoestore/walkthrough/instruction/InstructionViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/walkthrough/instruction/InstructionViewModel.kt
@@ -1,18 +1,5 @@
 package com.udacity.shoestore.walkthrough.instruction
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.udacity.shoestore.utils.SingleEvent
 
-class InstructionViewModel : ViewModel() {
-
-    private val _navigation = MutableLiveData<SingleEvent<Boolean>>()
-    val navigation: LiveData<SingleEvent<Boolean>>
-        get() = _navigation
-
-    fun navigation() {
-        _navigation.value = SingleEvent(true)
-    }
-
-}
+class InstructionViewModel : ViewModel()
